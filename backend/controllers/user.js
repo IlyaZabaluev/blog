1962ee8e2471f0527b20b83fcaf67ed9c9d5c12b
IlyaZabaluev <167705557+IlyaZabaluev@users.noreply.github.com>
2025-03-05 17:45:01 +0300

    Update user.js
    Looks like something went wrong!

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt')
-const User = require('../models/User')
+const User = require('../models/user')
 const { generate } = require('../helpers/token')
 const ROLES = require('../constants/roles')
 
